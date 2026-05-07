12d6b15c60833c216e1e736a7c953f242cb47292
Pranjal Amulani <pranjalamulani9@gmail.com>
2026-05-07 19:45:17 +0530

    Add SMTP timeouts

--- backend/utils/otpSender.js
+++ backend/utils/otpSender.js
@@ -11,7 +11,10 @@ if (process.env.SMTP_HOST && process.env.SMTP_USER) {
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
-        }
+        },
+        connectionTimeout: 10000, // 10 seconds
+        greetingTimeout: 10000,
+        socketTimeout: 10000
     });
 }
 
